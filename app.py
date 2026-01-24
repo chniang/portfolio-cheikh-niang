4042b77e23f7374c2fae6fe95c33e60a4cf44c34
chniang <cheikhniang159@gmail.com>
2026-01-24 21:49:20 +0000

    Disable caching completely to fix scroll bug on mobile

--- app.py
+++ app.py
@@ -5,16 +5,24 @@ app = Flask(__name__,
             static_folder='static',
             template_folder='.')
 
-# Configuration optimisée pour production
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000
+# DÉSACTIVER le cache pour les fichiers statiques
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    response = send_from_directory('.', 'index.html')
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return response
+
+@app.route('/test')
+def test():
+    return send_from_directory('.', 'test.html')
 
 @app.route('/static/<path:path>')
 def serve_static(path):
-    return send_from_directory('static', path)
+    response = send_from_directory('static', path)
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return response
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 7860))
