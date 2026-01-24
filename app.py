f163a6d6ee31b123b6f6f6e354a292bd4e87645c
chniang <cheikhniang159@gmail.com>
2026-01-24 16:43:15 +0000

    Fix scroll bug: cleanup observers on page navigation + optimize for mobile

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 ﻿from flask import Flask, send_from_directory
-from flask_compress import Compress
 import os
 
 app = Flask(__name__,
             static_folder='static',
             template_folder='.')
 
-# Activer la compression Gzip
-Compress(app)
-
 # Configuration optimisée pour production
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000
-app.config['COMPRESS_MIMETYPES'] = ['text/html', 'text/css', 'text/xml', 'application/json', 'application/javascript']
-app.config['COMPRESS_LEVEL'] = 6
-app.config['COMPRESS_MIN_SIZE'] = 500
 
 @app.route('/')
 def index():
